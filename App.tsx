91a1a875cbe2971cbc91b1ac3152052afcffa1a6
szczotszczot <szczotj@gmail.com>
2022-11-03 21:57:59 +0100

    add graphQL and apollo

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 
 import Tabs from "./src/components/Tabs";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {NavigationContainer} from "@react-navigation/native";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import {GRAPHQL_URL} from "./const";
+
 const Tab = createBottomTabNavigator();
+const client = new ApolloClient({
+    uri: `${GRAPHQL_URL}`, cache: new InMemoryCache(),
+});
+
 export default function App() {
-  return (
-      <NavigationContainer>
-         <Tabs/>
-      </NavigationContainer>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <NavigationContainer>
+                <Tabs/>
+            </NavigationContainer>
+        </ApolloProvider>
+    );
 }

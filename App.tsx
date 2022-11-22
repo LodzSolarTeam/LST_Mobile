8fd98d52c2d90cd71e18564a1b966de5bcde56e1
wisnievskikarol <wisnievskikarol@gmail.com>
2022-11-22 09:06:18 +0100

    statistics screen dev

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
-
 import Tabs from "./src/components/Tabs";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {NavigationContainer} from "@react-navigation/native";
-import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
-import {GRAPHQL_URL} from "./const";
+import { NavigationContainer } from "@react-navigation/native";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { GRAPHQL_URL } from "./const";
 
-const Tab = createBottomTabNavigator();
 const client = new ApolloClient({
-    uri: `${GRAPHQL_URL}`, cache: new InMemoryCache(),
+  uri: `${GRAPHQL_URL}`,
+  cache: new InMemoryCache(),
 });
 
 export default function App() {
-    return (
-        <ApolloProvider client={client}>
-            <NavigationContainer>
-                <Tabs/>
-            </NavigationContainer>
-        </ApolloProvider>
-    );
+  return (
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
+    </ApolloProvider>
+  );
 }

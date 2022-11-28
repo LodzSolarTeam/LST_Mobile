0e15fcb94df37d5db7af83b26a412456b84107b4
wisnievskikarol <wisnievskikarol@gmail.com>
2022-11-28 22:17:15 +0100

    padding added

--- src/components/home-screen/CarStatus.tsx
+++ src/components/home-screen/CarStatus.tsx
@@ -1,76 +1,71 @@
-import React, {useEffect, useState} from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const car = {
-    lightsOn : require("../../assets/carImg/car.png"),
-    lightsOff : require("../../assets/carImg/car_2.png"),
-    charging : require("../../assets/carImg/charging_car.png"),
-}
+  lightsOn: require("../../assets/carImg/car.png"),
+  lightsOff: require("../../assets/carImg/car_2.png"),
+  charging: require("../../assets/carImg/charging_car.png"),
+};
 
 interface ICarStatusProps {
-    status: boolean;
-    batteryPercentage: number;
+  status: boolean;
+  batteryPercentage: number;
 }
 
 export function CarStatus(props: ICarStatusProps) {
+  const [carImg, setCarImg] = useState(car.lightsOn);
+  useEffect(() => {
+    setCarImg(props.status ? car.lightsOn : car.lightsOff);
+  }, [props.status]);
 
-    const [carImg, setCarImg] = useState(car.lightsOn);
-    useEffect(() => {
-        setCarImg(props.status ? car.lightsOn : car.lightsOff)
-    }, [props.status]);
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.baseText}>
-                Welcome,
-                <Text style={styles.innerText}> Eagle Two</Text>
-            </Text>
-            <Text style={styles.batteryPercentageText}>
-                {props.batteryPercentage}%
-            </Text>
-            <Text style={styles.batteryText}>
-                BATTERY
-            </Text>
-            <Image
-                style={styles.batteryPercentageText}
-                source={carImg}
-            />
-            <View style={{marginTop : 40}}>
-                <Text style={styles.baseText}>
-                    Range,
-                    <Text style={styles.innerText}> 450km</Text>
-                </Text>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.baseText}>
+        Welcome,
+        <Text style={styles.innerText}> Eagle Two</Text>
+      </Text>
+      <Text style={styles.batteryPercentageText}>
+        {props.batteryPercentage}%
+      </Text>
+      <Text style={styles.batteryText}>BATTERY</Text>
+      <Image style={styles.batteryPercentageText} source={carImg} />
+      <View style={{ marginTop: 40 }}>
+        <Text style={styles.baseText}>
+          Range,
+          <Text style={styles.innerText}> 450km</Text>
+        </Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#060606',
-        color : '#FFF',
-        alignItems: 'center',
-        paddingBottom: 40
-    },
-    baseText: {
-        fontSize : 17,
-        color: '#FFF',
-        fontWeight: "300",
-    },
-    innerText: {
-        fontWeight: '600'
-    },
-    batteryPercentageText: {
-        fontWeight: '600',
-        color: '#FFF',
-        marginTop : 20,
-        fontSize : 32
-    },
-    car: {
-        marginTop : 30,
-    },
-    batteryText : {
-        fontSize : 10,
-        color : "#FFF"
-    },
+  container: {
+    backgroundColor: "#060606",
+    color: "#FFF",
+    alignItems: "center",
+    paddingBottom: 40,
+    paddingTop: 40,
+  },
+  baseText: {
+    fontSize: 17,
+    color: "#FFF",
+    fontWeight: "300",
+  },
+  innerText: {
+    fontWeight: "600",
+  },
+  batteryPercentageText: {
+    fontWeight: "600",
+    color: "#FFF",
+    marginTop: 20,
+    fontSize: 32,
+  },
+  car: {
+    marginTop: 30,
+  },
+  batteryText: {
+    fontSize: 10,
+    color: "#FFF",
+  },
 });
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
-import {CarStatus} from "../components/home-screen/CarStatus"
+import React from "react";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import { CarStatus } from "../components/home-screen/CarStatus";
 import TileSection from "../components/home-screen/TileSection";
 import useHomeScreenData from "../hooks/useHomeScreenData";
 
-
 export const HomeScreen = () => {
-    const homeScreenData = useHomeScreenData();
+  const homeScreenData = useHomeScreenData();
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView stickyHeaderIndices={[0]}>
-                <CarStatus status={homeScreenData?.isHighBeamLights!}
-                           batteryPercentage={homeScreenData?.batterySOC!}/>
-                <TileSection ambientTemperature={homeScreenData?.ambientTemperature!}
-                             interiorTemperature={homeScreenData?.interiorTemperature!}
-                             batterySOC={homeScreenData?.batterySOC!}
-                             CO2saved={homeScreenData?.CO2saved!}
-                             isHighBeamLights={homeScreenData?.isHighBeamLights!}
-                             estimatedRange={homeScreenData?.estimatedRange!}
-                             idealRange={homeScreenData?.idealRange!}/>
-            </ScrollView>
-        </SafeAreaView>
-    );
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView stickyHeaderIndices={[0]}>
+        <CarStatus
+          status={homeScreenData?.isHighBeamLights!}
+          batteryPercentage={homeScreenData?.batterySOC!}
+        />
+        <TileSection
+          ambientTemperature={homeScreenData?.ambientTemperature!}
+          interiorTemperature={homeScreenData?.interiorTemperature!}
+          batterySOC={homeScreenData?.batterySOC!}
+          CO2saved={homeScreenData?.CO2saved!}
+          isHighBeamLights={homeScreenData?.isHighBeamLights!}
+          estimatedRange={homeScreenData?.estimatedRange!}
+          idealRange={homeScreenData?.idealRange!}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#060606'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#060606",
+  },
 });

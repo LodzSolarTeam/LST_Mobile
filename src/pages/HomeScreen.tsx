151b754911c6c5d3a7142a7e0315644830185ca2
wisnievskikarol <wisnievskikarol@gmail.com>
2022-12-08 12:34:24 +0100

    added down arrow on main screen

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,19 +1,37 @@
-import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
 import { CarStatus } from "../components/home-screen/CarStatus";
 import TileSection from "../components/home-screen/TileSection";
 import useHomeScreenData from "../hooks/useHomeScreenData";
 
 export const HomeScreen = () => {
   const homeScreenData = useHomeScreenData();
+  const [top, setTop] = useState(0);
+
+  const handleScroll = (event: { nativeEvent: { contentOffset: number } }) => {
+    const { contentOffset } = event.nativeEvent;
+    setTop(contentOffset.y);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView stickyHeaderIndices={[0]}>
+      <ScrollView onScroll={handleScroll} stickyHeaderIndices={[0]}>
         <CarStatus
           status={homeScreenData?.isHighBeamLights!}
           batteryPercentage={homeScreenData?.batterySOC!}
         />
+        {!top && (
+          <View style={styles.arrowDown}>
+            <Image source={require("../assets/icons/arrow-down.png")} />
+          </View>
+        )}
+
         <TileSection
           ambientTemperature={homeScreenData?.ambientTemperature!}
           interiorTemperature={homeScreenData?.interiorTemperature!}
@@ -28,9 +46,22 @@ export const HomeScreen = () => {
   );
 };
 
+const containerBackgroundColor = "#060606";
+const arrowDownTop = 480;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#060606",
+    backgroundColor: containerBackgroundColor,
+  },
+  arrowDown: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: arrowDownTop,
+    left: 0,
+    right: 0,
+    zIndex: 100,
   },
 });
